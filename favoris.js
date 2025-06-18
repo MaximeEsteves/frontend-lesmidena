056ff23576b98562ce3962741bfec8719075d022
Maxime Esteves <maxime.esteves81@orange.fr>
2025-06-18 16:28:04 +0200

    maj favoris lien API

--- favoris.js
+++ favoris.js
@@ -10,7 +10,7 @@ mettreAJourBoutonsPanier();
 updateFavorisCount();
 });
 
-const baseURL = "https://backend-lesmidena-production.up.railway.app";
+const baseURL = "https://backend-lesmidena-production.up.railway.app/";
 
 // Fonction principale d'affichage des favoris
 function getProduitFavoris() {
